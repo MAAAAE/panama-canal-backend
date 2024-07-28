b2f9768cf49b832b10e5bdd78e9f19a2a0a3901c
d36choi <yuio1126@gmail.com>
2024-07-28 23:38:48 +0900

    feat: add get,post test in CategoryControllerTest

--- src/test/kotlin/io/maaaae/panama_canal/controller/CategoryControllerTest.kt
+++ src/test/kotlin/io/maaaae/panama_canal/controller/CategoryControllerTest.kt
@@ -1,21 +1,22 @@
 package io.maaaae.panama_canal.controller
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.core.spec.style.DescribeSpec
 import io.maaaae.panama_canal.controller.advice.GlobalExceptionHandler
+import io.maaaae.panama_canal.domain.Category
+import io.maaaae.panama_canal.dto.category.CategoryRequest
 import io.maaaae.panama_canal.service.category.CategoryService
 import io.mockk.every
 import io.mockk.mockk
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.get
+import org.springframework.test.web.servlet.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 
 
 @WebMvcTest(controllers = [CategoryController::class])
-class CategoryControllerTest: BehaviorSpec({
+class CategoryControllerTest : DescribeSpec({
 
     val categoryService: CategoryService = mockk<CategoryService>()
     val categoryController = CategoryController(categoryService)
@@ -25,18 +26,48 @@ class CategoryControllerTest: BehaviorSpec({
         .build()
 
 
-    given("category") {
+    describe("category") {
+
+        val categoryRequest: CategoryRequest = CategoryRequest(
+            name = "random",
+            domain = "domain",
+            description = "desc",
+            secret = "secret"
+        )
+
+        val category = Category(
+            categoryId = 1L,
+            name = "random",
+            description = "desc",
+            domain = "domain"
+
+        )
 
         every { categoryService.getAllCategories() } returns listOf()
+        every { categoryService.createCategory(categoryRequest) } returns
 
-        `when`("get all") {
+        context("GET /category") {
 
-            tmockMvc.get("/category") {
-                contentType = MediaType.APPLICATION_JSON
-                accept = MediaType.APPLICATION_JSON
-            }.andExpect {
-                status { MockMvcResultMatchers.status().isOk }
-                content { contentType(MediaType.APPLICATION_JSON) }
+            it("should return list of category") {
+
+                tmockMvc.get("/category") {
+                    contentType = MediaType.APPLICATION_JSON
+                    accept = MediaType.APPLICATION_JSON
+                }.andExpect {
+                    status { MockMvcResultMatchers.status().isOk }
+                }
+            }
+        }
+        context("POST /category") {
+
+            it("should create category") {
+
+                tmockMvc.post("/category") {
+                    contentType = MediaType.APPLICATION_JSON
+                    accept = MediaType.APPLICATION_JSON
+                }.andExpect {
+                    status { MockMvcResultMatchers.status().isBadRequest }
+                }
             }
         }
     }
