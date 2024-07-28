0ed3142e9fe9b3c91b5fd30c0c8dfb21939770bf
d36choi <yuio1126@gmail.com>
2024-07-29 00:20:42 +0900

    feat: implement Category Controller Mock Test
    - mockk + kotest
    - add Column unique constraint in category `name`

--- src/main/kotlin/io/maaaae/panama_canal/domain/Category.kt
+++ src/main/kotlin/io/maaaae/panama_canal/domain/Category.kt
@@ -1,6 +1,7 @@
 package io.maaaae.panama_canal.domain
 
 import io.maaaae.panama_canal.dto.category.CategoryRequest
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
@@ -12,6 +13,7 @@ import jakarta.persistence.Table
 data class Category(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val categoryId: Long = 0,
+    @Column(unique = true)
     var name: String,
     var description: String?,
     var domain: String,
