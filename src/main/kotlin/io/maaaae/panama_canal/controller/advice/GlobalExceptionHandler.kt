59c9b64a37b373a92fe1cf8e390af834c4bcbd68
Marc Lee <chlee1252@gmail.com>
2024-07-28 19:14:21 +0900

    feat: created error dto

--- src/main/kotlin/io/maaaae/panama_canal/controller/advice/GlobalExceptionHandler.kt
+++ src/main/kotlin/io/maaaae/panama_canal/controller/advice/GlobalExceptionHandler.kt
@@ -1,25 +1,39 @@
 package io.maaaae.panama_canal.controller.advice
 
+import io.maaaae.panama_canal.common.dto.PanamaErrorResponse
 import io.maaaae.panama_canal.common.exception.ResourceNotFoundException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.validation.FieldError
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
-import org.springframework.web.bind.support.WebExchangeBindException
 
 @RestControllerAdvice
 class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException::class)
-    fun handleValidationExceptions(ex: MethodArgumentNotValidException): ResponseEntity<Map<String, String?>> {
+    fun handleValidationExceptions(ex: MethodArgumentNotValidException): ResponseEntity<PanamaErrorResponse> {
         val errorMessage = ex.bindingResult.allErrors.firstOrNull()?.defaultMessage ?: "Invalid request"
-        val errorResponse = mapOf("errorCode" to HttpStatus.BAD_REQUEST.value().toString(), "errorMessage" to errorMessage)
+        val errorResponse = createErrorResponse(
+            errorCode = HttpStatus.BAD_REQUEST.value().toString(),
+            errorMessage = errorMessage
+        )
         return ResponseEntity(errorResponse, HttpStatus.BAD_REQUEST)
     }
 
     @ExceptionHandler(ResourceNotFoundException::class)
-    fun handleResourceNotFoundException(ex: ResourceNotFoundException): ResponseEntity<String> =
-        ResponseEntity(ex.message, HttpStatus.NOT_FOUND)
+    fun handleResourceNotFoundException(ex: ResourceNotFoundException): ResponseEntity<PanamaErrorResponse> =
+        ResponseEntity(
+            createErrorResponse(
+                errorCode = HttpStatus.NOT_FOUND.value().toString(),
+                errorMessage = ex.message,
+            ),
+            HttpStatus.NOT_FOUND)
+
+    private fun createErrorResponse(errorCode: String, errorMessage: String?): PanamaErrorResponse {
+        return PanamaErrorResponse(
+            errorCode = errorCode,
+            errorMessage = errorMessage,
+        )
+    }
 }
