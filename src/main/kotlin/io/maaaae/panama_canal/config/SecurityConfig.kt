c5e023835272d6cc86e23fe57e114822c1937bdf
d36choi <yuio1126@gmail.com>
2024-07-29 00:51:52 +0900

    fix: change webflux security config into WebSecurity config

--- src/main/kotlin/io/maaaae/panama_canal/config/SecurityConfig.kt
+++ src/main/kotlin/io/maaaae/panama_canal/config/SecurityConfig.kt
@@ -3,27 +3,32 @@ package io.maaaae.panama_canal.config
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.server.SecurityWebFilterChain
 
 
 @Configuration
-@EnableWebFluxSecurity
+//@EnableWebFluxSecurity
+@EnableWebSecurity
 class SecurityConfig {
 
     @Bean
-    fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
         http
-            .authorizeExchange { exchanges ->
+            .authorizeHttpRequests { exchanges ->
                 exchanges
-                    .pathMatchers("/actuator/**").permitAll()  // Actuator 엔드포인트는 공개
-                    .pathMatchers("/specs/**").permitAll() // Debug
-                    .anyExchange().authenticated()  // 그 외 모든 요청은 인증 필요
+                    .requestMatchers("/actuator/**").permitAll()  // Actuator 엔드포인트는 공개
+                    .requestMatchers("/specs/**").permitAll() // Debug
+                    .anyRequest().permitAll()  // 그 외 모든 요청은 인증 필요
             }
             .oauth2ResourceServer {
                 it.jwt(Customizer.withDefaults())
             }  // JWT를 통한 OAuth2 리소스 서버 설정
+
         return http.build()
     }
 
